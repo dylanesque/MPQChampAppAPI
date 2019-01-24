b6641b8bd549f787291560fc2473a21b57caa8ee
Michael Caveney <dylanesque27@gmail.com>
2019-01-23 22:48:32 -0500

    Began implementation and styling of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-    <div className="blue lighten"><h1 className="white-text">MPQ Champ Tracker</h1></div>
-     );
-}
+    <PageHeader>
+      <Heading>MPQ Champion Tracker</Heading>
+      <Navbar>
+
+      </Navbar>
+    </PageHeader>
+  );
+};
+
+const Heading = styled.h1`
+  color: white;
+  margin: 0;
+  padding: 2rem 1rem;
+`;
+
+const Navbar = styled.navbar`
+
+`;
+
+const NavLink = styled.li`
+  
+`;
+
+const PageHeader = styled.div`
+  background-color: #1a53ff;
+  display: flex;
+`;
 
 export default Header;
