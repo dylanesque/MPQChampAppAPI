18c0aaf34687f20b96dafa415328a39ad0a1cef7
Michael Caveney <dylanesque27@gmail.com>
2019-10-11 17:26:21 -0400

    Added express=prettify to make JSON output more human readable

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Imports required dependencies
 const express = require('express');
+const pretty = require('express-prettify');
+const seeds = require('./db.json');
 
 // Creates the Express app
 const app = express();
 
-const seeds = require('./db.json')
+// Middleware to pretty-print json
+app.use(pretty({ query: 'pretty' }));
 
-const database = app.get('/characters', (req, res) => {
+
+
+const database = app.get('/', (req, res) => {
   res.status(200).json(seeds);
 });
 
