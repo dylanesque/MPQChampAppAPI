680f3b81aada27008e34af25f28bde850a827e69
Michael Caveney <dylanesque27@gmail.com>
2019-10-11 17:17:43 -0400

    Added code to successfully render JSON db to browser

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const express = require('express');
 // Creates the Express app
 const app = express();
 
+const seeds = require('./db.json')
+
+const database = app.get('/characters', (req, res) => {
+  res.status(200).json(seeds);
+});
+
 // Starts the app
 const server = app.listen(3000, () => {
-  console.log("Test is working!");
+  console.log("This is working!");
+
 });
