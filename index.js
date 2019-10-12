47d078729baea89f3f29bb12ce53031fd31d2564
Michael Caveney <dylanesque27@gmail.com>
2019-10-12 12:40:34 -0400

    Populated seed database with all characters beginning with 'A'

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 // Imports required dependencies
-const express = require('express');
-const pretty = require('express-prettify');
-const seeds = require('./db.json');
+const express = require("express");
+const pretty = require("express-prettify");
+const seeds = require("./db.json");
 
 // Creates the Express app
 const app = express();
 
 // Middleware to pretty-print json
-app.use(pretty({ query: 'pretty' }));
+app.use(pretty({ query: "pretty" }));
 
-
-
-const database = app.get('/', (req, res) => {
+// Rendering logic for database
+const database = app.get("/", (req, res) => {
   res.status(200).json(seeds);
 });
 
 // Starts the app
 const server = app.listen(3000, () => {
   console.log("This is working!");
-
 });
